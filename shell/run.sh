7b179c17ffedb3eac3198d36ed84431b5444f493
谢超逸 <xcy0609@qq.com>
2016-08-19 15:31:54 +0800

    Update run.sh

--- shell/run.sh
+++ shell/run.sh
@@ -5,7 +5,7 @@ if [ ! -z "$FTP_USER" -a ! -z "$FTP_PASS" ]; then
     /ftpuser.sh -d "$FTP_USER" "$FTP_PASS"
 fi
 
-chmod 777 /var/ftp
+chmod -R 777 /var/ftp
 
 # Executing supervisord
 exec supervisord -n
